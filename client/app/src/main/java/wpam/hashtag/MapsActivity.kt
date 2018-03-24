88b450bd2c532f9ce2eac739e0bd5e78c965540d
Filip Rak <filiprak@wp.pl>
2018-03-25 00:56:54 +0100

    Add Google API MyLocation support.

--- client/app/src/main/java/wpam/hashtag/MapsActivity.kt
+++ client/app/src/main/java/wpam/hashtag/MapsActivity.kt
@@ -1,17 +1,32 @@
 package wpam.hashtag
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.design.widget.Snackbar
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import kotlinx.android.synthetic.main.activity_maps.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +36,70 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Snackbar.make(map.view!!, "Location services permissions required",
+                        Snackbar.LENGTH_INDEFINITE).setAction("Grant", object: View.OnClickListener {
+                    override fun onClick(view: View) {
+                        // Request the permission
+                        ActivityCompat.requestPermissions(this@MapsActivity,
+                                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+                    }
+                }).show()
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+            }
+        } else {
+            Log.i("PERMISSIONS", "requestLocationPermission(): permissions ACCESS_FINE_LOCATION already granted")
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    Log.i("PERMISSIONS", "permission ACCESS_FINE_LOCATION was granted")
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+
+                } else {
+                    Log.w("PERMISSIONS", "permission ACCESS_FINE_LOCATION was not granted")
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    finish()
+                }
+                return
+            }
+
+        // Add other 'when' lines to check for other
+        // permissions this app might request.
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
     /**
@@ -34,10 +113,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
+        requestLocationPermission()
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // ACCESS_FINE_LOCATION is granted
+            mMap.isMyLocationEnabled = true
+            mMap.setOnMyLocationButtonClickListener({
+                Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+                // Return false so that we don't consume the event and the default behavior still occurs
+                // (the camera animates to the user's current position).
+                return@setOnMyLocationButtonClickListener false
+            })
+            mMap.setOnMyLocationClickListener({ location ->
+                Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+            })
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    if (location != null)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude)))
+                }
+        }
     }
 }
