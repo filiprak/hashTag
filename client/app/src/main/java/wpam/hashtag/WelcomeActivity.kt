b6ea6f852edb677bd499021f9ef2a7970a6ae103
Filip Rak <filiprak@wp.pl>
2018-04-06 20:20:54 +0200

    Change MainActivity to WelcomeActivity. Add logo and basic styling.

---
+++ client/app/src/main/java/wpam/hashtag/WelcomeActivity.kt
@@ -0,0 +1,49 @@
+package wpam.hashtag
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+
+
+class WelcomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private var tag = "";
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        tag = GetMetaData(this, "log_tag") ?: ""
+        setupHandlers()
+    }
+
+    private fun setupHandlers() {
+        quitBtn.setOnClickListener(View.OnClickListener(::quitBtnHandler))
+        startBtn.setOnClickListener(View.OnClickListener(::startBtnHandler))
+        friendsBtn.setOnClickListener(View.OnClickListener(::friendsBtnHandler))
+        devBtn.setOnClickListener(View.OnClickListener(::devBtnHandler))
+    }
+
+    private fun startBtnHandler(view: View) {
+        startActivity(Intent(this, MapsActivity::class.java))
+    }
+
+    private fun quitBtnHandler(view: View) {
+        finish()
+    }
+
+    private fun friendsBtnHandler(view: View) {
+        TODO("not implemented")
+    }
+
+    private fun devBtnHandler(view: View) {
+        TODO("not implemented")
+    }
+}
