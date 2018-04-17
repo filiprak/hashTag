006d3d94a51725961dcfc28e314e0a37f473f23d
Filip Rak <filiprak@wp.pl>
2018-04-17 14:28:57 +0200

    Add clean and high contrast map styles.

--- client/app/src/main/java/wpam/hashtag/HashTagConfig.kt
+++ client/app/src/main/java/wpam/hashtag/HashTagConfig.kt
@@ -10,7 +10,9 @@ class HashTagConfig {
     // Map themes
     var mapThemes: HashMap<String, Int> = hashMapOf(
             "dark-blue" to R.raw.map_style_dblue,
-            "retro" to R.raw.map_style_retro
+            "retro" to R.raw.map_style_retro,
+            "clean" to R.raw.map_style_clean,
+            "contrast" to R.raw.map_style_contrast
     )
     var activeMapThemeId: Int = R.raw.map_style_retro
 
